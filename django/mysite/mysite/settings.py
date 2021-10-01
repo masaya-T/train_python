3262476f8370779cb40d844fc204b0bd8c5fd74a
masaya-T <tapgjtwmdkpl@gmail.com>
2021-10-01 14:29:09 +0900

    .gitignore is now working

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wyh)48@ij@i0316@(bq*fxpyn8sjuq#(16morbllen%2yq$p&@'
-
+from .local_settings import SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
