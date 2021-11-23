94a3018b2198e9f995386fe9b3a29b22f57a54ee
masaya-T <53888006+masaya-T@users.noreply.github.com>
2021-11-23 16:06:31 +0900

    Update settings.py

--- django/music_site/music_site/settings.py
+++ django/music_site/music_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p4t5#9kec@)5%ntpnt$mwy32ti9gea33pvaxy&117ya=a4v^7l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
